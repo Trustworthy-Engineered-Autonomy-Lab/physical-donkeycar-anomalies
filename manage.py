723460abc87ca5e05a7c5053c7c50a8d9bb56eb2
Jay Nibhanupudy <jaynibhanupudy@ufl.edu>
2026-03-23 17:17:18 -0400

    Added mud blob and friction loss anomalies

--- manage.py
+++ manage.py
@@ -76,6 +76,8 @@ def drive(cfg, model_path=None, use_joystick=False, model_type=None,
     cmd_latency = 0
     mass_scale = 1.0
     cam_pitch = 0.0
+    occlusion_fraction = .4
+    friction_scale = 1.0
 
     # Iterate over the meta list and assign values based on keys
     for item in meta:
@@ -106,6 +108,11 @@ def drive(cfg, model_path=None, use_joystick=False, model_type=None,
             mass_scale = float(value)
         elif key == "cam_pitch":
             cam_pitch = float(value)
+        elif key == "occlusion_fraction":
+            occlusion_fraction = float(value)
+        elif key == "friction_scale":
+            friction_scale = float(value)
+
 
     if gan_path and gan_type:
         model_type = "linear_with_gan" 
@@ -141,9 +148,9 @@ def drive(cfg, model_path=None, use_joystick=False, model_type=None,
     # if we are using the simulator, set it up
     #
     if env_name:
-        add_simulator(V, cfg, env_name, noise, name, folder_name=folder_name, num_drop = num_drop, brightness_coeff=brightness_coeff, cmd_latency = cmd_latency, mass_scale = mass_scale, cam_pitch = cam_pitch)
+        add_simulator(V, cfg, env_name, noise, name, folder_name=folder_name, num_drop = num_drop, brightness_coeff=brightness_coeff, cmd_latency = cmd_latency, mass_scale = mass_scale, cam_pitch = cam_pitch, occlusion_fraction = occlusion_fraction, friction_scale = friction_scale)
     else:
-        add_simulator(V, cfg, noise=noise, name=name, folder_name=folder_name, num_drop = num_drop, brightness_coeff=brightness_coeff, cmd_latency = cmd_latency, mass_scale = mass_scale, cam_pitch = cam_pitch)
+        add_simulator(V, cfg, noise=noise, name=name, folder_name=folder_name, num_drop = num_drop, brightness_coeff=brightness_coeff, cmd_latency = cmd_latency, mass_scale = mass_scale, cam_pitch = cam_pitch, occlusion_fraction = occlusion_fraction, friction_scale = friction_scale)
 
 
     #
@@ -839,7 +846,7 @@ def add_user_controller(V, cfg, use_joystick, input_image='ui/image_array'):
     return ctr
 
 
-def add_simulator(V, cfg, env_name = "", noise = "", name = "",folder_name="", num_drop = 0, brightness_coeff = 1.0, cmd_latency=0, mass_scale = 1.0, cam_pitch = 0.0):
+def add_simulator(V, cfg, env_name = "", noise = "", name = "",folder_name="", num_drop = 0, brightness_coeff = 1.0, cmd_latency=0, mass_scale = 1.0, cam_pitch = 0.0, occlusion_fraction = .4, friction_scale = 1.0):
     # Donkey gym part will output position information if it is configured
     # TODO: the simulation outputs conflict with imu, odometry, kinematics pose estimation and T265 outputs; make them work together.
     if cfg.DONKEY_GYM:
@@ -852,7 +859,7 @@ def add_simulator(V, cfg, env_name = "", noise = "", name = "",folder_name="", n
                            record_location=cfg.SIM_RECORD_LOCATION, record_gyroaccel=cfg.SIM_RECORD_GYROACCEL,
                            record_velocity=cfg.SIM_RECORD_VELOCITY, record_lidar=cfg.SIM_RECORD_LIDAR,
                         #    record_distance=cfg.SIM_RECORD_DISTANCE, record_orientation=cfg.SIM_RECORD_ORIENTATION,
-                           delay=cfg.SIM_ARTIFICIAL_LATENCY, num_drop=num_drop, name=name, folder_name=folder_name, brightness_coeff =brightness_coeff, cmd_latency = cmd_latency, mass_scale = mass_scale, cam_pitch = cam_pitch)
+                           delay=cfg.SIM_ARTIFICIAL_LATENCY, num_drop=num_drop, name=name, folder_name=folder_name, brightness_coeff =brightness_coeff, cmd_latency = cmd_latency, mass_scale = mass_scale, cam_pitch = cam_pitch, occlusion_fraction = occlusion_fraction, friction_scale = friction_scale)
         threaded = True
         inputs = ['steering', 'throttle']
         outputs = ['cam/image_array']
